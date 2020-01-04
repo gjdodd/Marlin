a45f3f67ae5d7790cb504f103479d5a5840ecf2d
gjdodd <graham.dodd@airintake.co.uk>
2020-01-04 20:12:26 +0000

    Include correct nozzle z offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -932,7 +932,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 10, 10, -1.2 }
+#define NOZZLE_TO_PROBE_OFFSET { 10, 10, -2.7 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
